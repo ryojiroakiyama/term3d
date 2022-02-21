ed34ac3618ebe309bde76d14457d751260b2d758
nao215912 <naoki3.14159@gmail.com>
2022-02-21 20:11:28 +0900

    add: key_event

--- main.c
+++ main.c
@@ -1,25 +1,68 @@
 #include "term3d.h"
 #include <strings.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <termios.h>
+#include <stdlib.h>
+
+int kbhit(void) {
+  struct termios oldt, newt;
+  int ch;
+  int oldf;
+
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+  ch = getchar();
+
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+  if (ch != EOF) {
+    ungetc(ch, stdin);
+    return 1;
+  }
+
+  return 0;
+}
+
+void key_event(t_draw *draw){
+  int c;
+  if (kbhit()) {
+    c = getchar();
+    if(c == 'q')
+      exit(EXIT_SUCCESS);
+    else if (c == 'w')
+      draw->scale += 0.2;
+    else if (c == 's')
+      draw->scale -= 0.2;
+  }
+}
 
 int	main(void)
 {
 	t_draw		draw;
 
 	draw.map_size = W_MAP * H_MAP;
-	draw.scale = 1;
 	draw.vecs = string_to_vectors(read_file("./torus.3d"), &(draw.vecs_size));
 	iterate_vectors(&draw, get_average);
 	matrix_translate(draw.matrix4, \
 					-1 * draw.vecs_ave.x, 0, -1 * draw.vecs_ave.z);
 	iterate_vectors(&draw, do_matrix);
+        draw.scale = 4;
 	while (1)
 	{
 		bzero(draw.map, sizeof(draw.map));
 		matrix_rotate_y(draw.matrix4);
-		draw.scale = 4;
 		iterate_vectors(&draw, mapping);
 		put_map(draw.map, draw.map_size);
 		usleep(USLEEP);
+                key_event(&draw);
 	}
+        return (0);
 }
