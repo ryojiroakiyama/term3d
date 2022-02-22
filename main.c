bb66af4bc98894221157c2d5f48d892f0875fb99
rakiyama <ryojiro25@gmail.com>
2022-02-22 12:29:59 +0900

    norm and zero y

--- main.c
+++ main.c
@@ -6,55 +6,57 @@
 #include <termios.h>
 #include <stdlib.h>
 
-int kbhit(void) {
-  struct termios oldt, newt;
-  int ch;
-  int oldf;
+int	kbhit(void)
+{
+	struct termios	oldt;
+	struct termios	newt;
+	int				ch;
+	int				oldf;
 
-  tcgetattr(STDIN_FILENO, &oldt);
-  newt = oldt;
-  newt.c_lflag &= ~(ICANON | ECHO);
-  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-
-  ch = getchar();
-
-  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-  fcntl(STDIN_FILENO, F_SETFL, oldf);
-
-  if (ch != EOF) {
-    ungetc(ch, stdin);
-    return 1;
-  }
-
-  return 0;
+	tcgetattr(STDIN_FILENO, &oldt);
+	newt = oldt;
+	newt.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+	oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+	fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+	ch = getchar();
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+	fcntl(STDIN_FILENO, F_SETFL, oldf);
+	if (ch != EOF)
+	{
+		ungetc(ch, stdin);
+		return (1);
+	}
+	return (0);
 }
 
-void key_event(t_draw *draw){
-  int c;
-  if (kbhit()) {
-    c = getchar();
-    if(c == 'q')
-      exit(EXIT_SUCCESS);
-    else if (c == 'w')
-      draw->scale += 0.2;
-    else if (c == 's')
-      draw->scale -= 0.2;
-  }
+void	key_event(t_draw *draw)
+{
+	int	c;
+
+	if (kbhit())
+	{
+		c = getchar();
+		if (c == 'q')
+			exit(EXIT_SUCCESS);
+		else if (c == 'w')
+			draw->scale += 0.2;
+		else if (c == 's')
+			draw->scale -= 0.2;
+	}
 }
 
 int	main(void)
 {
-	t_draw		draw;
+	t_draw	draw;
 
 	draw.map_size = W_MAP * H_MAP;
 	draw.vecs = string_to_vectors(read_file("./torus.3d"), &(draw.vecs_size));
 	iterate_vectors(&draw, get_average);
 	matrix_translate(draw.matrix4, \
-					-1 * draw.vecs_ave.x, 0, -1 * draw.vecs_ave.z);
+		-1 * draw.vecs_ave.x, -1 * draw.vecs_ave.y, -1 * draw.vecs_ave.z);
 	iterate_vectors(&draw, do_matrix);
-        draw.scale = 4;
+	draw.scale = 4;
 	while (1)
 	{
 		bzero(draw.map, sizeof(draw.map));
@@ -62,7 +64,7 @@ int	main(void)
 		iterate_vectors(&draw, mapping);
 		put_map(draw.map, draw.map_size);
 		usleep(USLEEP);
-                key_event(&draw);
+		key_event(&draw);
 	}
-        return (0);
+	return (0);
 }
