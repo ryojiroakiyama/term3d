e465af55b50e0e95420fb7a42475b41ab15c745a
nao215912 <naoki3.14159@gmail.com>
2022-02-22 20:07:52 +0900

    fix: test.sh

--- test.sh
+++ test.sh
@@ -5,12 +5,12 @@ make > /dev/null
 err_files="./3dfiles/error/*"
 
 for file in $err_files; do
-  echo q | ./term3D $file 2>> log.txt > /dev/null
+  echo q | ./term3D $file 2> log.txt > /dev/null
   if [ -s log.txt ]; then
     printf -- "${file}\t\033[32mOK\x1b[0m \n"
   else
     printf -- "${file}\t\e[31mNG\x1b[0m \n"
   fi
 done
-#
-#rm log.txt
+
+rm log.txt
